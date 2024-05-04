02a1710569533ec19aaa109ecffa7a09f3c323a6
madaooftheblues <azlanrasool@gmail.com>
2024-05-04 14:14:36 +0500

    Integrate picocss

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css'
+import './css/pico.min.css'
 import observer from './modules/observer.js'
 import weatherAPI from './modules/weatherAPI.js'
 import cityInputForm from './modules/DOM/cityInputForm.js'
