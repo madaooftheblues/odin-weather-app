8c12bed282db91687087fccdb76380f76892beb5
madaooftheblues <azlanrasool@gmail.com>
2024-05-04 09:03:07 +0500

    Add Observer.js

---
+++ src/modules/observer.js
@@ -0,0 +1,20 @@
+export default class Observer {
+    events = {}
+
+    subscribe(eName, fn) {
+        this.events[eName] = this.events[eName] || []
+        this.events[eName].push(fn)
+    }
+
+    unsubscribe(eName, fn) {
+        if (!(eName in this.events)) return
+        this.events[eName] = this.events[eName].filter((f) => f !== fn)
+    }
+
+    publish(eName, data) {
+        if (!(eName in this.events)) return
+        this.events[eName].forEach((fn) => {
+            fn(data)
+        })
+    }
+}
