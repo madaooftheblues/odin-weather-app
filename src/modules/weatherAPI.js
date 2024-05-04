82d7d7e31af4638dc346b5f10266fae6681394e9
madaooftheblues <azlanrasool@gmail.com>
2024-05-04 10:54:25 +0500

    Hide Weather API key

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,7 +1,7 @@
-export default async function fetchWeatherData(city) {
-    const API_KEY = '21ad2e4143f14cc4b7d25350243004'
-    const baseURL = 'http://api.weatherapi.com/v1'
+const API_KEY = process.env.WEATHER
+const baseURL = 'http://api.weatherapi.com/v1'
 
+export default async function fetchWeatherData(city) {
     const res = await fetch(`${baseURL}/current.json?key=${API_KEY}&q=${city}`)
     const data = await res.json()
 
