c2f2fcf58af340c33762f0578825b37856d7d4a2
madaooftheblues <azlanrasool@gmail.com>
2024-05-05 08:27:36 +0500

    Update weatherAPI.js to include forecast days

--- src/modules/weatherAPI.js
+++ src/modules/weatherAPI.js
@@ -1,8 +1,11 @@
 const API_KEY = process.env.WEATHER
 const baseURL = 'http://api.weatherapi.com/v1'
+const forecastDays = 3
 
 export default async function fetchWeatherData(city) {
-    const res = await fetch(`${baseURL}/current.json?key=${API_KEY}&q=${city}`)
+    const res = await fetch(
+        `${baseURL}/forecast.json?key=${API_KEY}&q=${city}&days=${forecastDays}`,
+    )
     const data = await res.json()
 
     console.log(data)
